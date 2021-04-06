6337e2290379f5616c24359040e87b8515c8750a
rvishwas21 <rvishwas16@gmail.com>
2021-04-06 15:49:11 +0530

    tnpa2

--- NestedArray/fyp_TNPA.m
+++ NestedArray/fyp_TNPA.m
@@ -11,12 +11,12 @@ d2=(M1+1)*d1;
 SNR=20;
 fs=10*fc;
 t=0:1/fs:4999*(1/fs);
-S=[cos(2*pi*1e6*t').*cos(2*pi*fc*t') cos(2*pi*2e6*t').*cos(2*pi*fc*t') cos(2*pi*3e6*t').*cos(2*pi*fc*t)'];
+S=[cos(2*pi*2e6*t').*cos(2*pi*fc*t') cos(2*pi*3e6*t').*cos(2*pi*fc*t)'];
 K=size(S,2);%no. of signals
 sigmansq=10^(-SNR/10);
 n1=sqrt(sigmansq)*(randn(length(t),M)+1i*randn(length(t),M));
 n2=sqrt(sigmansq)*(randn(length(t),M)+1i*randn(length(t),M));
-signal_angle=[10 40 80; 10 30 40];
+signal_angle=[40 80; 10 30];
 A1=getManifoldMatrixA1(signal_angle,M1,M2,d1,lambda);
 Phi=getPhi(signal_angle,d,lambda);
 A2=A1*Phi;
@@ -24,13 +24,16 @@ x1=S*A1'+n1;
 x2=S*A2'+n2;
 
 R11=cov(x1,1);
+[~,nv]=eigs(R11,M);
+sigmanest=mean(nv(K+1:M));
+R11 = R11 - sigmanest*eye(M);
 z1=R11(:);
-z1b=z1(getIndexOfUniqueElements(M1,M2)); %zib = va bar
+z1b=flip(z1(getIndexOfUniqueElements(M1,M2))); %zib = va bar
 R21=(x2'*x1)/length(t);
 
 
 z2=R21(:);
-z2b=z2(getIndexOfUniqueElements(M1,M2)); %z2b = vb bar
+z2b=flip(z2(getIndexOfUniqueElements(M1,M2))); %z2b = vb bar
 Mb=M2*(M1+1);
 
 c = z1b(1:Mb);
@@ -45,37 +48,29 @@ Vb = hankel(c,r);
 
 D = Vb*pinv(Va);
 
-[F,psi] = eigs(D,Mb)
+[F,psi] = eigs(D,Mb);
 
-u = getUvector(F,Mb,K,d,lambda);
-v = getVvector(psi,Mb,K,d1,lambda);
+u = getUvector(F,Mb,Mb,d,lambda)
+v = getVvector(psi,Mb,d1,lambda)
 
-ThetaMat = asin(sqrt(u.^2+v.^2))*180/pi;
-PhiMat = angle(u+v*1i)*180/pi;
+ThetaMat = asin(sqrt(u.^2 + v.^2))*180/pi
+PhiMat = angle(u+v*1i)*180/pi
 
 
 function U = getUvector(F,Mb,K,d,lambda)
-sum = 0
-dr = 2*pi*d/lambda
+dr = 2*pi*d/lambda;
 U = [];
 for k=1:K
-   for m=1:Mb-1
-        sum = sum + angle(F(k,m+1)/F(k,m))/dr;
-   end
-   uk = sum/(Mb-1);
-   U = [U uk];   
+   U(k) = (1/((Mb-1)*dr))*(sum(angle(F(2:Mb,k)./F(1:Mb-1,k))));
 end
-U = U';
 end
 
-function V = getVvector(psi,Mb,K,d1,lambda)
+function V = getVvector(psi,K,d1,lambda)
 dr = 2*pi*d1/lambda;
 V = [];
 for k=1:K
-    vk = angle(psi(k,k))/dr;
-    V = [V vk];
+    V(k) = angle(psi(k,k))/dr;
 end
-V = V';
 end
 
 function uidx=getIndexOfUniqueElements(M1,M2)
